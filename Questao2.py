3adb733c85fe0cf8461b6ae768dd73513fa32aed
Kakauvict <kauanyvict1@gmail.com>
2026-05-25 11:19:56 -0300

    Implement sum calculation for user input number

---
+++ Questao2.py
@@ -0,0 +1,12 @@
+print("\nQUESTÃO 2")
+
+n = int(input("Digite um número: "))
+
+contador = 1
+soma = 0
+
+while contador <= n:
+    soma = soma + contador
+    contador += 1
+
+print("A soma é:", soma)
