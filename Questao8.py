0728fba64f19404bad18f88d28772d14fa15a8ad
Kakauvict <kauanyvict1@gmail.com>
2026-05-25 11:28:37 -0300

    Add script to print even numbers in a range

---
+++ Questao8.py
@@ -0,0 +1,11 @@
+print("\nQUESTÃO 8")
+
+inicio = int(input("Digite o início: "))
+fim = int(input("Digite o fim: "))
+
+while inicio <= fim:
+
+    if inicio % 2 == 0:
+        print(inicio)
+
+    inicio += 1
