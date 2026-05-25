38aa983e92f56de9ba20d13728e031001577e802
Kakauvict <kauanyvict1@gmail.com>
2026-05-25 11:20:17 -0300

    Add multiplication table functionality in Questao3.py

---
+++ Questao3.py
@@ -0,0 +1,10 @@
+print("\nQUESTÃO 3")
+
+numero = int(input("Digite um número: "))
+
+contador = 1
+
+while contador <= 10:
+    resultado = numero * contador
+    print(numero, "x", contador, "=", resultado)
+    contador += 1
