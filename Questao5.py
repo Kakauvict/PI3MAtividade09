40a57b9d5dc157d8474430e360c91401991c7325
Kakauvict <kauanyvict1@gmail.com>
2026-05-25 11:21:25 -0300

    Add a program to sum input numbers until zero

---
+++ Questao5.py
@@ -0,0 +1,11 @@
+print("\nQUESTÃO 5")
+
+soma = 0
+
+numero = int(input("Digite um número (0 para parar): "))
+
+while numero != 0:
+    soma = soma + numero
+    numero = int(input("Digite outro número (0 para parar): "))
+
+print("Soma total:", soma)
